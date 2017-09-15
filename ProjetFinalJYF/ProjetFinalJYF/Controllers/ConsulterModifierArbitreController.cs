bb0bc870cfa633bb8a9facfe229f307fbcf4adb2
teddj33 <julien.gornet@gmail.com>
2017-09-15 10:26:33 +0200

    ConsulterMofifierArbitre

---
+++ ProjetFinalJYF/ProjetFinalJYF/Controllers/ConsulterModifierArbitreController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetFinalJYF.Controllers
+{
+    public class ConsulterModifierArbitreController : Controller
+    {
+        private List<string> ListeNiveau = null;
+        private List<Arbitre> ListeArbitre = null;
+
+        public ActionResult ConsulterModifier()
+        {
+            return View();
+
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ConsulterModifierArbitreController()
+        {
+            ListeNiveau = new List<string>
+                    {
+
+                    "Tous les niveaux",
+                    "Federal",
+                        "Stagiaire",
+                       "ACF",
+                       "Territorial",
+                       "PreFederal"
+                    };
+            ListeArbitre = new List<Arbitre>
+        {
+            new Arbitre {Nom="Orias",Prénom="Fred", NiveauArbitre= ListeNiveau[1],Club="Le Lou",Téléphone="0698563214",Adresse="Vernaison",DDN= new DateTime(1987,09,16)},
+            new Arbitre {Nom="Despinasse",Prénom="Yo", NiveauArbitre= ListeNiveau[1],Club="ST",Téléphone="0632547854",Adresse="Toulouse",DDN= new DateTime(1986,09,15)},
+            new Arbitre {Nom="Gornet",Prénom="Ju", NiveauArbitre= ListeNiveau[2],Club="UBB",Téléphone="0654785412",Adresse="Bordeaux",DDN= new DateTime(1997,12,16)},
+            new Arbitre {Nom="Arnaud",Prénom="Jo",NiveauArbitre= ListeNiveau[3],Club="UBB",Téléphone="0654785412",Adresse="Bordeaux",DDN= new DateTime(1997,12,16)},
+            new Arbitre {Nom="Cherbal",Prénom="Amin", NiveauArbitre= ListeNiveau[2],Club="Le Lou",Téléphone="0321456398",Adresse="Lyon",DDN= new DateTime(1942,02,18)},
+            new Arbitre {Nom="Makri",Prénom="Ali", NiveauArbitre= ListeNiveau[4],Club="Clermont",Téléphone="0547896523",Adresse="Clermont",DDN= new DateTime(1987,02,12)},
+        };
+
+        }
+        [HttpGet]
+        public Arbitre[] Afficher(NiveauxFiltre f)
+        {
+            return ListeArbitre.ToArray();
+        }
+        
+
+        //public Arbitre[] Post([FromBody]string niveau)
+        //{
+        //    return ListeArbitre.Where(p => p.NiveauArbitre == niveau).ToArray();
+
+        //}
+
+    }
+    public class NiveauxFiltre
+    {
+        public bool Federal;
+    }
+
+    public class Arbitre
+    {
+        public string Nom { get; set; }
+        public string Prénom { get; set; }
+        public string NiveauArbitre { get; set; }
+        public string Club { get; set; }
+        public string Téléphone { get; set; }
+        public string Adresse { get; set; }
+        public DateTime DDN { get; set; }
+    }
+    public class Federal
+    {
+        public bool federal { get; set; }
+    }
+
+
+
+
+}
---
+++ ProjetFinalJYF/ProjetFinalJYF/Scripts/ConsulterModifierArbitre.js
@@ -0,0 +1,35 @@
+﻿
+$('#BtNiveau').on('click', function () {
+    var data = {};//les données du coté serveur, controller.cs
+
+    data.Federal = document.getElementById('cb_Federal').checked;
+    data.Stagiaire = document.getElementById('cb_Stagiaire').checked;
+    data.ACF = document.getElementById('cb_ACF').checked;
+    data.Territorial = document.getElementById('cb_Territorial').checked;
+    data.PreFederal = document.getElementById('cb_PreFederal').checked;
+    $.ajax({
+        url: "http://localhost:55189/ConsulterModifierArbitre/ConsulterModifier",
+        type: 'Get',
+        contentType: 'application/json; charset=UTF-8',
+        datatype: 'json',
+        data: data, //data coté client qu'on a ci-dessous
+        success: function (data, status) {
+
+            var html = '<table id="tableau"><caption>Liste des Arbitres</caption><tr class="nomColonne"><th>Nom</th><th>Prenom</th><th>NiveauArbitre</th><th>Club</th><th>Téléphone</th><th>Adresse</th><th>DDN</th><th>Modifier</th><th>Supprimer</th></tr>';
+            for (i = 0; i < data.length; i++) {
+                html += '<tr><td>' + data[i].Nom + '</td><td>' + data[i].Prénom + '</td><td>' + data[i].NiveauArbitre + '</td><td>'
+                    + data[i].Club + '</td><td>' + data[i].Téléphone + '</td><td>' + data[i].Adresse + '</td><td>' + data[i].DDN + '</td><td>' +
+                    '<input type="button" id="modifierArbitre" onclick="modifierArbitre()" style="color: green;" value="Modifier" />' + '</td><td>' +
+                    '<input type="button" style="background: url(Images/images.png)" id="supprimerArbitre" onclick="supprimerArbitre()" />' + '</td></tr>';
+            }
+            //<input type="button" id="supprimerArbitre" onclick="supprimerArbitre()" style="color: red;" value="Supprimer" />
+
+            html += '</table>';
+            $('#tableauDiv').html(html);
+
+        },
+        Error: function (resultat, status, error) { $('#test').html(error); }
+    });
+    //$('#tableau').css('background-color', 'orange');
+});
+
