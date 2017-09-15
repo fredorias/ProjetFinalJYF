f2bc81875cef716ae334d2b561d886deb3c4ab2a
YDespi <yolande.despinasse@gmail.com>
2017-09-15 16:42:56 +0200

    Calendrier 15-16h40

---
+++ ProjetFinalJYF/ProjetFinalJYF/Controllers/CalendrierController.cs
@@ -0,0 +1,71 @@
+﻿using ProjetFinalJYF.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetFinalJYF.Controllers
+{
+    public class CalendrierController : Controller
+    {
+        public Repository CalRepo = new Repository();
+
+        // GET: Calendrier
+        public ActionResult Index()
+        {
+            return View(CalRepo.GetAllMatch());
+        }
+        //[HttpGet]
+        //public ActionResult Create()
+        //{
+        //    var match = new Match();
+        //    return View(match);
+        //}
+        //[HttpPost]
+        //public ActionResult Create(Match newMatch)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        int n = CalRepo.Add(newMatch);
+        //        if (n == 0)
+        //            return RedirectToAction("error");
+        //        return RedirectToAction("Index");
+
+        //    }
+        //    return View(newMatch);
+        //}
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            //Affiche le formulaire
+            var match = CalRepo.GetAllMatch().Where(t => t.MatchId == id).FirstOrDefault();
+            if (match == null)
+            {
+                return RedirectToAction("index");
+            }
+            else
+                return View(match);
+
+        }
+        [HttpPost]
+        public ActionResult Edit(Match matchDuFormulaire)
+        {
+            var match = CalRepo.GetAllMatch().Where(t => t.MatchId == matchDuFormulaire.MatchId).FirstOrDefault();
+            if (match == null)
+            {
+                return RedirectToAction("error");
+            }
+            if (ModelState.IsValid)
+            {
+                CalRepo.Update(match, matchDuFormulaire);
+                return View(match);
+
+            }
+            else
+                return View(match);
+
+        }
+
+    }
+}
--- ProjetFinalJYF/ProjetFinalJYF/Data/Repository.cs
+++ ProjetFinalJYF/ProjetFinalJYF/Data/Repository.cs
@@ -8,5 +8,29 @@ namespace ProjetFinalJYF.Data
     public class Repository
     {
         private ArbitreContext Context = new ArbitreContext();
+
+        internal IEnumerable<Match> GetAllMatch()
+        {
+            return Context.Matchs.Include("AdresseMatch").Include("ArbitreMatch").Include("CalendrierMatch");
+        }
+
+        internal int Add(Match newMatch)
+        {
+
+            Context.Matchs.Add(newMatch);
+            return Context.SaveChanges();
+        }
+        internal void Update(Match matchToUpdate, Match newData)
+        {
+            matchToUpdate.Visiteurs = newData.Visiteurs;
+            matchToUpdate.Locaux = newData.Locaux;
+            matchToUpdate.HeureMatch = newData.HeureMatch;
+            matchToUpdate.NiveauMatch = newData.NiveauMatch;
+            matchToUpdate.AdresseMatch = newData.AdresseMatch;
+            matchToUpdate.ArbitreMatch = newData.ArbitreMatch;
+            matchToUpdate.CalendrierMatch = newData.CalendrierMatch;
+
+            Context.SaveChanges();
+        }
     }
 }
