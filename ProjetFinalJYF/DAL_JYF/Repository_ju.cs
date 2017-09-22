ce03efb72355f7aaf2f7aaaebc4b9c36b5a310a0
teddj33 <julien.gornet@gmail.com>
2017-09-22 10:54:52 +0200

    création page edit et finalisation consulter arbitre

---
+++ ProjetFinalJYF/DAL_JYF/Repository_ju.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_JYF
+{
+    public partial class Repository
+    {
+        public IEnumerable<Arbitre> GetAllArbitre()
+        {
+            return Context.Arbitres.Include("AdresseArb");
+        }
+    }
+}
--- ProjetFinalJYF/ProjetFinalJYF/Controllers/ConsulterModifierArbitreController.cs
+++ ProjetFinalJYF/ProjetFinalJYF/Controllers/ConsulterModifierArbitreController.cs
@@ -19,31 +19,6 @@ namespace ProjetFinalJYF.Controllers
             return View();
 
         }
-
-        //public ConsulterModifierArbitreController()
-        //{
-        //    ListeNiveau = new List<string>
-        //            {
-
-        //            "Tous les niveaux",
-        //            "Federal",
-        //                "Stagiaire",
-        //               "ACF",
-        //               "Territorial",
-        //               "PreFederal"
-        //            };
-
-        //    ListeArbitre = new List<Arbitre>
-        //{
-        //    new Arbitre {Nom="ORIAS",Prenom="Fred", NiveauArbitre= ListeNiveau[1],Club="Le Lou",Telephone="0698563214",AdresseVille="Vernaison",DateNaissance= new DateTime(1987,09,16)},
-        //    new Arbitre {Nom="DESPINASSE",Prenom="Yo", NiveauArbitre= ListeNiveau[1],Club="ST",Telephone="0632547854",AdresseVille="Toulouse",DateNaissance= new DateTime(1986,09,15)},
-        //    new Arbitre {Nom="GORNET",Prenom="Ju", NiveauArbitre= ListeNiveau[2],Club="UBB",Telephone="0654785412",AdresseVille="Bordeaux",DateNaissance= new DateTime(1997,12,16)},
-        //    new Arbitre {Nom="ARNAUD",Prenom="Jo",NiveauArbitre= ListeNiveau[3],Club="UBB",Telephone="0654785412",AdresseVille="Bordeaux",DateNaissance= new DateTime(1997,12,16)},
-        //    new Arbitre {Nom="CHERBAL",Prenom="Amin", NiveauArbitre= ListeNiveau[2],Club="Le Lou",Telephone="0321456398",AdresseVille="Lyon",DateNaissance= new DateTime(1942,02,18)},
-        //    new Arbitre {Nom="MAKRI",Prenom="Ali", NiveauArbitre= ListeNiveau[4],Club="Clermont",Telephone="0547896523",AdresseVille="Clermont",DateNaissance= new DateTime(1987,02,12)},
-        //};
-
-        //}
         [HttpGet]
         public ActionResult Afficher(NiveauxFiltre selectionNiveau)
         {
@@ -97,6 +72,20 @@ namespace ProjetFinalJYF.Controllers
                                           )
             .ToArray(), JsonRequestBehavior.AllowGet);
         }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
 
+            //var match = CalRepo.GetAllMatch().Where(t => t.MatchId == id).FirstOrDefault();
+            //var arbdispo = CalRepo.GetArbDispo(match.CalendrierMatch.DateJournee);
+            //ViewBag.ListeArbitresDispo = arbdispo;
+            //if (match == null)
+            //{
+            //    return RedirectToAction("index");
+            //}
+            //else
+                return View(); /*match*/
+
+        }
     }
 }
