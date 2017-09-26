b8fc9a149314ea719a7306dcd5edb5f24e718c45
YDespi <yolande.despinasse@gmail.com>
2017-09-26 10:37:44 +0200

    service arb

---
+++ ProjetFinalJYF/ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WebScraper.WCF;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("TestArbitreService");
+            // Client WCF
+            ChannelFactory<IArbitreService> Canal2 = new ChannelFactory<IArbitreService>("CanalArbitre");
+            IArbitreService serv2 = Canal2.CreateChannel();
+
+            var r = serv2.GetAllMatchsByQueryName("Arbitres");
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ ProjetFinalJYF/WCFContract/ModelContract/DataContract.cs
@@ -0,0 +1,86 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScraper.WCF
+{
+    [DataContract]
+    public class QueryContract
+    {
+        [DataMember]
+        public Guid Id { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
+        public DateTime? DataExpiryDate { get; set; }
+        [DataMember]
+        public DateTime? DataTimeStamp { get; set; }
+        [DataMember]
+        public List<PageContract> ListePages { get; set; }
+
+    }
+
+    [DataContract]
+    public class PageContract
+    {
+        [DataMember]
+        public Guid Id { get; set; }
+
+        [DataMember]
+        public virtual QueryContract Query { get; set; }
+        [DataMember]
+        public string URL { get; set; }
+        [DataMember]
+        public List<SelectorContract> ListeSelectors { get; set; }
+
+    }
+    [DataContract]
+    public class SelectorContract
+    {
+        [DataMember]
+        public Guid Id { get; set; }
+        [DataMember]
+        public virtual PageContract Page { get; set; }
+        [DataMember]
+        public string Value { get; set; }
+
+    }
+    [DataContract]
+    public class ResultsHeaderContract
+    {
+        public ResultsHeaderContract()
+        {
+
+        }
+        public ResultsHeaderContract(SelectorContract SC)
+        {
+            Id = Guid.NewGuid();
+            QueryExecutionDate = DateTime.Now;
+            Selector = SC;
+        }
+        [DataMember]
+        public Guid Id { get; set; }
+        [DataMember]
+        public virtual SelectorContract Selector { get; set; }
+        [DataMember]
+        public DateTime QueryExecutionDate { get; set; }
+    }
+    [DataContract]
+    public class ResultsDetailContract
+    {
+        [DataMember]
+        public Guid Id { get; set; }
+        [DataMember]
+        public virtual ResultsHeaderContract ResultsHeader { get; set; }
+        [DataMember]
+        public string Value { get; set; }
+        [DataMember]
+        public string CLEF { get; set; }
+    }
+}
