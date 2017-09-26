9b10d6ab27b26ca247384dcb095691e0d7f13f41
teddj33 <julien.gornet@gmail.com>
2017-09-26 13:53:19 +0200

    ju

--- ProjetFinalJYF/ProjetFinalJYF/Scripts/ConsulterModifierArbitre.js
+++ ProjetFinalJYF/ProjetFinalJYF/Scripts/ConsulterModifierArbitre.js
@@ -24,13 +24,13 @@ $('#BtNiveau').on('click', function () {
         data: dataClient, //data coté client qu'on a ci-dessous
         success: function (data, status) {
 
-            var html = '<table id="tableau" class="table"><caption>Liste des Arbitres</caption><tr class="nomColonne"><th>Nom</th><th>Prenom</th><th>NiveauArbitre</th><th>Club</th><th>Téléphone</th><th>Adresse</th><th>Date de naissance</th><th>Modifier</th><th>Supprimer</th></tr>';
+            var html = '<table id="tableau" class="table"><caption>Liste des Arbitres</caption><tr class="nomColonne"><th>Nom</th><th>Prenom</th><th>NiveauArbitre</th><th>Club</th><th>Téléphone</th><th>Adresse</th><th>Modifier</th><th>Supprimer</th></tr>';
             for (i = 0; i < data.length; i++) {
                 html += '<tr><td>' + data[i].Nom + '</td><td>' + data[i].Prenom + '</td><td>' + data[i].NiveauArbitre + '</td><td>'
                     + data[i].Club + '</td><td>' + data[i].Telephone + '</td><td>' +
                      (data[i].AdresseArb == null?"":data[i].AdresseArb.Ville) +
-                    '</td><td>' + (data[i].DateNaissance) + '</td><td>' +
-                    '<a id="modifierArbitre" onclick="modifierArbitre()" style="color: green;" href="/NouvelArbitre/EditerArbitre/' + data[i].ArbitreId + '">Modifier</a>' + '</td><td>' +
+                    '</td><td>' +
+                    '<a id="modifierArbitre" onclick="modifierArbitre()" style="color: lightgreen;" href="/NouvelArbitre/EditerArbitre/' + data[i].ArbitreId + '">Modifier</a>' + '</td><td>' +
                     '<a "id="supprimerArbitre" onclick="supprimerArbitre()" style="color: red" style="cursor: pointer"; href="/ConsulterModifierArbitre/supprimerArbitre/' + data[i].ArbitreId + '">Supprimer</a>' + '</td></tr>';
 
             }
