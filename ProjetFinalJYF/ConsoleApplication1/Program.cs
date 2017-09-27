4d0aa5d89c76cda3cb5862755f4a875b2439bdc4
YDespi <yolande.despinasse@gmail.com>
2017-09-27 09:24:14 +0200

    pre sourceMatch

--- ProjetFinalJYF/ConsoleApplication1/Program.cs
+++ ProjetFinalJYF/ConsoleApplication1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL_JYF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
@@ -8,23 +9,55 @@ using WebScraper.WCF;
 
 namespace ConsoleApplication1
 {
-    class Program
+    public class SourceMatchWeb
     {
-        static void Main(string[] args)
+       private Repository rep = null;
+        
+        public void SourceMatch()
         {
-
-
-            Console.WriteLine("TestArbitreService");
-            // Client WCF
+            rep = new Repository();
+            
             ChannelFactory<IArbitreService> Canal2 = new ChannelFactory<IArbitreService>("CanalArbitre");
             IArbitreService serv2 = Canal2.CreateChannel();
 
-            var r = serv2.GetAllMatchsByQueryName("Arbitres");
+            var resultsServer = serv2.GetAllMatchsByQueryName("Arbitres");
 
-            Console.ReadLine();
+            resultsServer.Select(r => r.NiveauMatch).ToList();
 
+            List<DAL_JYF.Match> newArbL = resultsServer.Select(r => 
+                new DAL_JYF.Match() {
+                    Locaux=r.Locaux,
+                    Visiteurs=r.Visiteurs,
+                    HeureMatch=r.HeureMatch,
+                    CalendrierMatch= r.CalendrierMatch == null ? null : new DAL_JYF.Calendrier() { DateJournee=r.CalendrierMatch.DateJournee },
+                    AdresseMatch= r.AdresseMatch == null ? null : new DAL_JYF.Adresse() { NomAdresse=r.AdresseMatch.NomAdresse },
+                    NiveauMatch= Convertir(r.NiveauMatch),
+                }
 
+                ).ToList();
+            
+        rep.AddRange(newArbL);
+            
+        }
 
+        private static string Convertir(string value)
+        {
+            if (value == null)
+                return "";
+            if (value.Equals("Balandrade - Championnat Territorial"))
+                return "Série1";
+            if (value.Equals("Compétition Libre"))
+                return "Promotion d'honneur";
+            if (value.Equals("Teuliere - Championnat Territorial"))
+                return "Série2";
+            if (value.Equals("Réserves Honneur - Championnat Territorial"))
+                return "Série3";
+            if (value.Equals("Fédérale 1 Féminine"))
+                return "Honneur";
+            if (value.Equals("Phliponeau B - Championnat Territorial"))
+                return "Série4";
+
+            return value;
         }
     }
 }
